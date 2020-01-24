2198b2c043ee851ab1ab17696f956f3c3b23abe6
JASON GUTIERREZ <jasongutierrez1989@gmail.com>
2020-01-24 00:45:30 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,37 @@
-// Your code here
+class Cat {
+  constructor(name, sex) {
+    this.name = name;
+    this.sex = sex;
+  }
+
+  function speak(name) {
+    return `${name} says meow!`;
+  }
+}
+
+class Dog {
+  constructor(name, sex) {
+    this.name = name;
+    this.sex = sex;
+  }
+
+  function speak(name) {
+    return `${name} says woof!`;
+  }
+}
+
+class Bird {
+  constructor(name, sex) {
+    this.name = name;
+    this.sex = sex;
+  }
+
+  function speak(name, sex) {
+    if (sex === 'male') {
+      return `It's me! ${name}, the parrot!`;
+    }
+    else {
+      return `${name} says squawk!`;
+    }
+  }
+}
